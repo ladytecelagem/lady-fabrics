655bd99299aa4c09aa250cf3674c9fa534c38dd7
ladytecelagem <luis@ladyfabrics.com>
2026-05-23 21:38:37 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export default function middleware(_req: NextRequest) {
-  return NextResponse.next();
+export default function middleware(req: NextRequest) {
+  const headers = new Headers(req.headers);
+  headers.set("x-pathname", req.nextUrl.pathname);
+  return NextResponse.next({ request: { headers } });
 }
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|studio|.*\\..*).*)"],
 };
+
 
